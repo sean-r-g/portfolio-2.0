ac1fa85f25544b08c927e4f08eaf334f7ee26142
Sean Gillis <seangillis@Seans-MBP.attlocal.net>
2022-07-20 10:45:27 -0500

    re adding resume without mobile number

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -14,7 +14,7 @@ const NavBar = ({handleShowHome, handleShowAbout}) => {
                 <Nav className="me-auto">
                 <Nav.Link href='#about'>About</Nav.Link>
                 <Nav.Link href='#projects'>My Work</Nav.Link>
-                <Nav.Link href="">Experience</Nav.Link>
+                <Nav.Link href="https://drive.google.com/file/d/1trkHvPrEfU-TsN-0tg4gWU41gaR0ro2L/view?usp=sharing">Experience</Nav.Link>
                 {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                     <NavDropdown.Item>Action</NavDropdown.Item>
                     <NavDropdown.Item>Another action</NavDropdown.Item>
